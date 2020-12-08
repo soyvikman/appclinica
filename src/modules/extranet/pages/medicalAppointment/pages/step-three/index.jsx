a43151b1f33309e4843c30f115ded9dca937ec2e
Tahirih <>
2020-12-08 10:37:12 -0500

    Add react form and get data from form

--- src/modules/extranet/pages/medicalAppointment/pages/step-three/index.jsx
+++ src/modules/extranet/pages/medicalAppointment/pages/step-three/index.jsx
@@ -1,29 +1,52 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Layout from '../../../../components/Layout';
 import MaterialInput from '../../../../../../global/components/MaterialInput';
+import { OutlinedSelectContainer } from '../../../../../../global/components/MaterialSelect';
+import MaterialSelect from '../../../../../../global/components/MaterialSelect';
 import { useHistory } from 'react-router-dom';
+
+import './steThree.css';
+
+const departmentList = [
+  { "textContent": "Arequipa", "value": "00", "shortContent": "Arequipa" },
+  { "textContent": "Lima", "value": "01", "shortContent": "Lima" },
+  { "textContent": "Tacna", "value": "04", "shortContent": "Tacna" }
+];
 
 const StepThree = () => {
   const history = useHistory();
-  const nextStep = () => {
-    console.log('save');
+  const { register, handleSubmit } = useForm();
+
+  const nextStep = (e) => {
+    console.log('save', e);
   }
   return (
     <Layout step={3}>
-      step 3
-      <div className="mt-4">
+      <form className="mt-4" onSubmit={handleSubmit(nextStep)}>
         <div className="row d-flex justify-content-between">
-          <MaterialInput
-            className="col-12 col-md-6"
-            placeholder="Nombres"
-          />
+          <OutlinedSelectContainer>
+            <MaterialSelect
+              className="col-12 col-md-6"
+              placeholder="Departamento"
+              selectOptions={departmentList}
+              name="department"
+              register={register}
+            />
+          </OutlinedSelectContainer>
           <MaterialInput
             className="col-12 col-md-6 mt-4 mt-md-0"
-            placeholder="Apellidos"
+            name="specialty"
+            register={register}
+            placeholder="Especialidad"
           />
         </div>
-        <button onClick={nextStep}>Continuar</button>
-      </div>
+        <div className="row">
+          <div className="col-12 text-center mt-3">
+            <button className="btn btn-lg btn-skyblue" type="submit">Generar cita</button>
+          </div>
+        </div>
+      </form>
     </Layout>
   );
 };
--- src/modules/extranet/pages/medicalAppointment/pages/step-two/index.jsx
+++ src/modules/extranet/pages/medicalAppointment/pages/step-two/index.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 import Layout from '../../../../components/Layout';
 import MaterialInput from '../../../../../../global/components/MaterialInput';
 
+import './stepTwo.css';
+
 const StepTwo = () => {
   const history = useHistory();
-  const nextStep = () => {
+  const { register, handleSubmit } = useForm();
+  const nextStep = (e) => {
+    console.log(e);
     history.push('/home/citas/step-3')
   }
 
   return (
     <Layout step={2}>
-      <div className="mt-4">
-        paso 2
-        <div className="row d-flex justify-content-between">
+      <form className="mt-4" onSubmit={handleSubmit(nextStep)}>
+        <div className="row d-flex justify-content-center">
           <MaterialInput
-            className="col-12 col-md-6"
-            placeholder="Nombres"
+            className="col-12 col-md-8"
+            placeholder="Teléfono"
+            name="phoneNumber"
+            register={register}
           />
           <MaterialInput
-            className="col-12 col-md-6 mt-4 mt-md-0"
-            placeholder="Apellidos"
+            className="col-12 col-md-8 mt-3"
+            placeholder="Correo electrónico"
+            name="email"
+            register={register}
           />
         </div>
-        <button onClick={nextStep}>Continuar</button>
-      </div>
+        <div className="row">
+          <div className="col-12 text-center mt-3">
+            <button className="btn btn-lg btn-skyblue" type="submit">Continuar</button>
+          </div>
+        </div>
+      </form>
     </Layout>
   );
 };
