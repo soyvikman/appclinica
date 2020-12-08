1fbb2ee40256bd90beafd4aa489792498dae700c
Victor Castro <soyvikman@gmail.com>
2020-12-08 12:54:11 -0500

    feat: login

--- src/modules/extranet/pages/login/components/formLogin/index.jsx
+++ src/modules/extranet/pages/login/components/formLogin/index.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './formLogin.css';
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
+import axios from "axios";
 
 const FormLogin = () => {
   const history = useHistory();
   const { register, handleSubmit } = useForm();
+  const [loginError, gettingLogingError] = useState({display: "none"});
 
-  const onSubmit = (value) => {
-    console.log(value);
+  const onSubmit = async (value) => {
+    try{
+      const data = await axios.post('http://localhost:5000/validarLogin', {
+        inputUser: value.userName,
+        inputPassword: value.psw
+      })
+      if(!data.data.error){
+        localStorage.setItem('usuario', JSON.stringify(data.data.usuario))
+      } else {
+        gettingLogingError({display: "block"})
+      }
+    } catch(e){
+      console.log(e)
+    }
+    
   }
   return (
     <div className="container">
@@ -18,6 +33,9 @@ const FormLogin = () => {
             <input type="text" placeholder="Usuario" name="userName" ref={register}/>
             <input type="password" placeholder="Contraseña" name="psw" ref={register}/>
             <button type="submit">Iniciar sesión</button>
+              <div className="alert alert-danger" style={loginError} role="alert">
+                La contraseña proporcionada es incorrecta.
+              </div>
           </form>
         </div>
       </div>
